87d975731aa13d0868f6372ff67e6c683e4a1cec
nilllzz <nidresch@googlemail.com>
2018-12-06 22:55:52 +0100

    add two helper funcs to common

--- lib/PEngine.Common/Data/DataHelper.cs
+++ lib/PEngine.Common/Data/DataHelper.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PEngine.Common.Data
 {
@@ -9,6 +11,26 @@ namespace PEngine.Common.Data
             // upper case first char
             member = member[0].ToString().ToUpper() + member.Substring(1);
             return (T)Enum.Parse(typeof(T), member);
+        }
+
+        public static string UnparseEnum(object enumMember)
+        {
+            var str = enumMember.ToString();
+            return str[0].ToString().ToLower() + str.Substring(1);
+        }
+
+        public static int GetFirstFreeInRange(IEnumerable<int> range, int first = 0)
+        {
+            var max = range.Max();
+            for (var i = first; i < max; i++)
+            {
+                if (!range.Contains(i))
+                {
+                    return i;
+                }
+            }
+            // end of range is first free
+            return max + 1;
         }
     }
 }
